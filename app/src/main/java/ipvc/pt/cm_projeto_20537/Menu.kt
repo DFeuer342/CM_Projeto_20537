00cfc56d3b47525308db04a3ae9d2e9640ef80d7
TiagoRebelo <tgr342r@gmail.com>
2021-04-15 21:24:01 +0100

    Criação layout menu

--- app/src/main/java/ipvc/pt/cm_projeto_20537/Menu.kt
+++ app/src/main/java/ipvc/pt/cm_projeto_20537/Menu.kt
@@ -2,10 +2,15 @@ package ipvc.pt.cm_projeto_20537
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
     }
+
+    fun onClickMenuMapa(view: View) {}
+    fun onClickMenuNotas(view: View) {}
+    fun onClickMenuAbout(view: View) {}
 }
