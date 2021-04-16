ef3e4b8fcb2ce1646d40ba820f57b7b904d2268e
TiagoRebelo <tgr342r@gmail.com>
2021-04-16 01:59:08 +0100

    Querys

--- app/src/main/java/ipvc/pt/cm_projeto_20537/DAO/NotasDao.kt
+++ app/src/main/java/ipvc/pt/cm_projeto_20537/DAO/NotasDao.kt
@@ -1,4 +1,21 @@
 package ipvc.pt.cm_projeto_20537.DAO
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ipvc.pt.cm_projeto_20537.ententies.Nota
+
+@Dao
 interface NotasDao {
+
+        @Query("SELECT * FROM nota_table ORDER BY id DESC")
+        fun getAlphabetizedNotas(): LiveData<List<Nota>>
+
+        @Insert(onConflict = OnConflictStrategy.IGNORE)
+        suspend fun insert(nota: Nota)
+
+        @Query("DELETE FROM nota_table WHERE id= :id")
+        suspend fun deleteAll(id: Int?)
 }
