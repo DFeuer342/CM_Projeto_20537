05aab64340cff6e40afdb3f6d87b4ff73b0c8a9f
TiagoRebelo <tgr342r@gmail.com>
2021-04-16 03:03:46 +0100

    NotaDB correção

--- app/src/main/java/ipvc/pt/cm_projeto_20537/db/NotaDB.kt
+++ app/src/main/java/ipvc/pt/cm_projeto_20537/db/NotaDB.kt
@@ -1,4 +1,40 @@
 package ipvc.pt.cm_projeto_20537.db
 
-class NotaDB {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ipvc.pt.cm_projeto_20537.DAO.NotasDao
+import ipvc.pt.cm_projeto_20537.ententies.Nota
+import kotlinx.coroutines.CoroutineScope
+
+
+@Database(entities = [Nota::class], version = 1, exportSchema = false)
+  public abstract class NotaRoomDatabase : RoomDatabase() {
+
+    abstract fun notasDao(): NotasDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NotaRoomDatabase? = null
+
+
+        fun getDatabase(context: Context, scope: CoroutineScope): NotaRoomDatabase {
+            val tempInstance = INSTANCE
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NotaRoomDatabase::class.java,
+                    "notas_database"
+                )
+                    .build()
+
+                INSTANCE = instance
+                // return instance
+                return instance
+            }
+        }
+    }
 }
