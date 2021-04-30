112e4e6ad22e80150ba567f5331695c332d3f456
TiagoRebelo <tgr342r@gmail.com>
2021-05-01 00:54:11 +0100

    update addNotas ListaNotas. Adicionar uma nova nota e listagem correta

--- app/src/main/java/ipvc/pt/cm_projeto_20537/AddNotas.kt
+++ app/src/main/java/ipvc/pt/cm_projeto_20537/AddNotas.kt
@@ -7,11 +7,47 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class AddNotas : AppCompatActivity() {
+
+    private lateinit var insertTituloView: EditText
+    private lateinit var insertDescricaoView: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notas)
+
+        insertTituloView = findViewById(R.id.addNotaTitulo)
+        insertDescricaoView = findViewById(R.id.addNotaDescricao)
+
+        val btCancelar = findViewById<Button>(R.id.btCancelar)
+        btCancelar.setOnClickListener {
+            Toast.makeText(this, "Operação cancelada", Toast.LENGTH_SHORT).show()
+            finish();
+        }
+
+        val btGuardar = findViewById<Button>(R.id.btGuardarNota)
+        btGuardar.setOnClickListener{
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(insertTituloView.text) && TextUtils.isEmpty(insertDescricaoView.text)){
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val id = insertTituloView.id
+                replyIntent.putExtra(EXTRA_REPLY_ID, id)
+                val titulo = insertTituloView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY_TITLE, titulo)
+                val descricao = insertDescricaoView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY_CONTENT, descricao)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+
+    }
+    companion object {
+        const val EXTRA_REPLY_ID = "id"
+        const val EXTRA_REPLY_TITLE = "titulo"
+        const val EXTRA_REPLY_CONTENT  = "descricao"
     }
 }
--- app/src/main/java/ipvc/pt/cm_projeto_20537/ListaNotas.kt
+++ app/src/main/java/ipvc/pt/cm_projeto_20537/ListaNotas.kt
@@ -1,7 +1,9 @@
 package ipvc.pt.cm_projeto_20537
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -10,10 +12,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ipvc.pt.cm_projeto_20537.adapters.NotaAdapter
+import ipvc.pt.cm_projeto_20537.ententies.Nota
 import ipvc.pt.cm_projeto_20537.viewModel.NotaViewModel
 
 class ListaNotas : AppCompatActivity() {
-    //private lateinit var NotaViewModel: NotaViewModel
     private val newWordActivityRequestCode = 1;
 
     val NotaViewModel: NotaViewModel by viewModels()
@@ -29,13 +31,32 @@ class ListaNotas : AppCompatActivity() {
 
         val viewModel = ViewModelProvider(this).get(NotaViewModel::class.java)
         NotaViewModel.allNotas.observe(this, Observer { notas ->
-            notas?.let{ adapter.setNotas(it)}
+            notas?.let { adapter.setNotas(it) }
         })
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener{
-            val intent = Intent(this@ListaNotas, Menu::class.java)
+        fab.setOnClickListener {
+            val intent = Intent(this@ListaNotas, AddNotas::class.java)
             startActivityForResult(intent, newWordActivityRequestCode)
         }
+
+        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == newWordActivityRequestCode) {
+                if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+                    var titulo = data?.getStringExtra(AddNotas.EXTRA_REPLY_TITLE).toString()
+                    var descricao = data?.getStringExtra(AddNotas.EXTRA_REPLY_CONTENT).toString()
+                    var nota = Nota(titulo = titulo, descricao = descricao)
+                    NotaViewModel.insert(nota)
+                    Toast.makeText(this, "Nota guardada.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(
+                            applicationContext,
+                            "Dados vazios. Operação cancelada",
+                            Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
+
 }
