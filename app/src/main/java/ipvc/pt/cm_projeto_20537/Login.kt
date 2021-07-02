b74e8e3b697632fb10271cd790d940a2ba001e0e
TiagoRebelo <tgr342r@gmail.com>
2021-07-02 18:55:35 +0100

    Editado Layout da atividade login

--- app/src/main/java/ipvc/pt/cm_projeto_20537/Login.kt
+++ app/src/main/java/ipvc/pt/cm_projeto_20537/Login.kt
@@ -2,10 +2,13 @@ package ipvc.pt.cm_projeto_20537
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    fun btLogin(view: View) {}
 }
